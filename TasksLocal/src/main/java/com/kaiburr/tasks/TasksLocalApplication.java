a6620541c8b84544e30563dcf3fabc3eca0046a4
Jietthesh Balaji <mljb413@gmail.com>
2024-01-20 16:09:06 +0530

    Uploading the source code with YAML files in Resources Directory

---
+++ TasksLocal/src/main/java/com/kaiburr/tasks/TasksLocalApplication.java
@@ -0,0 +1,13 @@
+package com.kaiburr.tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksLocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksLocalApplication.class, args);
+	}
+
+}
---
+++ TasksLocal/src/test/java/com/kaiburr/tasks/TasksLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaiburr.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksLocalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
